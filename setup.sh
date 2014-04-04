267b504deef1c91b9ee9e692ae16715484b5f145
Ken Guest <ken@linux.ie>
2014-04-04 16:03:13 +0100

    other files, before I lose them.

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# setup.sh -- created 18-May-2012, Ken Guest
+# @Last Change: 18-May-2012
+# @Revision:    0.0
+
+if [ ! -f ~/.vimrc ] ; then
+	ln -sv `pwd` ~/.vimrc
+else
+	echo "~/.vimrc already exists"
+fi
+if [ ! -f ~/.vim ] ; then
+	ln -sv `pwd` ~/.vim
+else
+	echo "~/.vim already exists"
+fi
